352e5118b4548a395ef5ef44e8c9a63f72d2cd67
Federico Hidalgo <federico.hidalgo@uelectronics.comgit config --global user.email federico.hidalgo@uelectronics.com>
2022-11-10 12:49:28 -0600

    10/11/2022

--- Practica 4 - Input ()/Input ().py
+++
@@ -1,9 +0,0 @@
-#Entrada por teclado input()
-
-#Declaramos variables de almacenamiento
-a = input("¿Como te llamas? ")
-b = input("¿Cual es tu edad? ")
-
-#Imprimimos el resultado
-print ("Hola ", a, "!")
-print ("Tu edad es de ", b, " años")
